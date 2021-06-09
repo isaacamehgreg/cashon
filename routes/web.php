5b91c99a04b485bc3b453954c1a83401b364cc0c
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-06-09 03:08:29 -0700

    heroku

--- routes/web.php
+++ routes/web.php
@@ -29,125 +29,26 @@ Route::get('/logout', function () {
     Auth::logout();
     return redirect('login');
 });
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
-
-// })->name('dashboard');
 
 Route::get('/dashboard', function () {
+    $play = DB::table('games_played')->get(); 
+
     if(Auth::user()->role == 'agent'){
         return view('agent.index')->with(['play',$play]);
     }
-    $play = DB::table('games_played')->get(); 
+    
     return view('dashboard')->with(['play',$play]);
-});
-
-
-
-
-Route::get('result', function () {
-    
-     $most =   DB::table('games_played')
-            ->select('number')
-            ->groupBy('number')
-            ->orderByRaw('COUNT(*) DESC')
-            ->limit(5)
-            ->get();
-
-    $least =   DB::table('games_played')
-                ->select('number')
-                ->groupBy('number')
-                ->orderByRaw('COUNT(*) ASC')
-                ->limit(5)
-                ->get();
-
-    dd($least);
-    
 
 });
 
-//all games played
-Route::get('/bets',function(){
- return Bet::all();
-
-});
-// pick a winer
-Route::get('/winners', function(){
-    //get the winning number
-    $winning_number =   DB::table('games_played')->select('number')->groupBy('number')->orderByRaw('COUNT(*) ASC')->limit(5)->get();      
-        
-    $magic_number= array( $winning_number[0]->number=>$winning_number[0]->number, 
-                          $winning_number[1]->number=>$winning_number[1]->number,
-                          $winning_number[2]->number=>$winning_number[2]->number,
-                          $winning_number[3]->number=>$winning_number[3]->number,
-                          $winning_number[4]->number=>$winning_number[4]->number);
-    $magic= $winning_number[0]->number. 
-            $winning_number[1]->number.
-            $winning_number[2]->number.
-            $winning_number[3]->number.
-            $winning_number[4]->number;
-    //magic number is just winnng numbers in key value pair so that Arr::exists can work and search
-
-     //for loop throw all the tickes
-      $bets = Bet::all();
-      foreach($bets as $bet){
-          
-        echo $bet->bet_code.'_'.$winning_number[0]->number,$winning_number[1]->number,$winning_number[2]->number, $winning_number[3]->number,$winning_number[4]->number.'||||||||||';
-        //compare using arr exist while counting and updating a collumn of combination:
-         
-         $result= DB::table('bets')->where('bet_code',$bet->bet_code)->update(['result'=>$magic,'status'=>'played',]);
-
-          $count = 0;
-        
-
-        if($exists = Arr::exists($magic_number, $bet->n1)){  //if the number matches 
-            
-            $count = $count+1;
-            //update the bet ticket status
-        
-        }
-        if($exists = Arr::exists($magic_number, $bet->n2)){  //if the number matches 
-            
-            $count = $count+1;
-            //update the bet ticket status
-         
-        }
-        if($exists = Arr::exists($magic_number, $bet->n3)){  //if the number matches 
-          
-            
-            //update the bet ticket status
-          
-        }
-        if($exists = Arr::exists($magic_number, $bet->n4)){  //if the number matches 
-           
-            $count = $count+1;
-            //update the bet ticket status
-           
-        }
-        if($exists = Arr::exists($magic_number, $bet->n5)){  //if the number matches 
-           
-            $count = $count+1;
-            //update the bet ticket status
-          
-        }
-        DB::table('bets')->where('bet_code',$bet->bet_code)->update(['combination'=>$count]);
-        echo 'done';
-        
-       
-      
 
 
-        //compare if n1 wins if n2 wings
-        
-
-
-      }
-
-});
 
 
 
@@ -174,7 +75,7 @@ Route::get('all_terminal',[AgentController::class,'all_terminal']);
 
 //sms
 Route::get('sms', function () {
-    return '12345';
+    
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
@@ -201,3 +102,103 @@ Route::get('sms', function () {
 
 });
 
+
+
+//...................................the game logic....................//
+Route::get('result', function () {
+    
+    $most =   DB::table('games_played')
+           ->select('number')
+           ->groupBy('number')
+           ->orderByRaw('COUNT(*) DESC')
+           ->limit(5)
+           ->get();
+
+   $least =   DB::table('games_played')
+               ->select('number')
+               ->groupBy('number')
+               ->orderByRaw('COUNT(*) ASC')
+               ->limit(5)
+               ->get();
+
+   dd($least);
+   
+
+});
+
+//all games played
+Route::get('/bets',function(){
+return Bet::all();
+
+});
+// pick a winer
+Route::get('/winners', function(){
+   //get the winning number
+   $winning_number =   DB::table('games_played')->select('number')->groupBy('number')->orderByRaw('COUNT(*) ASC')->limit(5)->get();      
+       
+   $magic_number= array( $winning_number[0]->number=>$winning_number[0]->number, 
+                         $winning_number[1]->number=>$winning_number[1]->number,
+                         $winning_number[2]->number=>$winning_number[2]->number,
+                         $winning_number[3]->number=>$winning_number[3]->number,
+                         $winning_number[4]->number=>$winning_number[4]->number);
+   $magic= $winning_number[0]->number. 
+           $winning_number[1]->number.
+           $winning_number[2]->number.
+           $winning_number[3]->number.
+           $winning_number[4]->number;
+   //magic number is just winnng numbers in key value pair so that Arr::exists can work and search
+
+    //for loop throw all the tickes
+     $bets = Bet::all();
+     foreach($bets as $bet){
+         
+       echo $bet->bet_code.'_'.$winning_number[0]->number,$winning_number[1]->number,$winning_number[2]->number, $winning_number[3]->number,$winning_number[4]->number.'||||||||||';
+       //compare using arr exist while counting and updating a collumn of combination:
+        
+        $result= DB::table('bets')->where('bet_code',$bet->bet_code)->update(['result'=>$magic,'status'=>'played',]);
+
+         $count = 0;
+       
+
+       if($exists = Arr::exists($magic_number, $bet->n1)){  //if the number matches 
+           
+           $count = $count+1;
+           //update the bet ticket status
+       
+       }
+       if($exists = Arr::exists($magic_number, $bet->n2)){  //if the number matches 
+           
+           $count = $count+1;
+           //update the bet ticket status
+        
+       }
+       if($exists = Arr::exists($magic_number, $bet->n3)){  //if the number matches 
+         
+           
+           //update the bet ticket status
+         
+       }
+       if($exists = Arr::exists($magic_number, $bet->n4)){  //if the number matches 
+          
+           $count = $count+1;
+           //update the bet ticket status
+          
+       }
+       if($exists = Arr::exists($magic_number, $bet->n5)){  //if the number matches 
+          
+           $count = $count+1;
+           //update the bet ticket status
+         
+       }
+       DB::table('bets')->where('bet_code',$bet->bet_code)->update(['combination'=>$count]);
+       echo 'done';
+       
+       //compare if n1 wins if n2 wings
+       
+
+     }
+
+});
+
+
+//..........................................
