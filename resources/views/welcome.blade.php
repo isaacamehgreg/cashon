f28a178097586e2b34e8b4067843dfd7434af72e
Isaac Ameh <67932483+isaacamehgreg@users.noreply.github.com>
2021-06-11 00:54:42 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,8 +17,8 @@
               html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,
               video{display:block;vertical-align:middle}
               video{max-width:100%;height:auto}
-              .bg-white{--bg-opacity:1;background-color:#000;background-color:rgba(255,255,255,var(--bg-opacity))}
-              .bg-gray-100{--bg-opacity:1;
+              .bg-white{--bg-opacity:1;background-color:#000;background-color:rgba(255,0,0,var(--bg-opacity))}
+              .bg-gray-100{--bg-opacity:2;
                              background-color:#ff0000;
                              background-color:rgba(247,250,252,var(--bg-opacity))
                              }
